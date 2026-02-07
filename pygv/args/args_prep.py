184bb1339a1759d9d9e52e58dcbaf952275da645
varghele <mgm.fischer@gmail.com>
2026-02-07 13:33:28 +0100

    added new embedding defaults

--- pygv/args/args_prep.py
+++ pygv/args/args_prep.py
@@ -26,19 +26,19 @@ def add_prep_args(parser: argparse.ArgumentParser):
     discovery_group = parser.add_argument_group('State Discovery Options')
     discovery_group.add_argument('--discover_states', action='store_true',
                                   help='Run Graph2Vec + clustering for unsupervised state discovery')
-    discovery_group.add_argument('--g2v_embedding_dim', type=int, default=64,
+    discovery_group.add_argument('--g2v_embedding_dim', type=int, default=512,
                                   help='Graph2Vec embedding dimension')
-    discovery_group.add_argument('--g2v_max_degree', type=int, default=2,
+    discovery_group.add_argument('--g2v_max_degree', type=int, default=3,
                                   help='Graph2Vec WL iteration depth')
     discovery_group.add_argument('--g2v_epochs', type=int, default=50,
                                   help='Graph2Vec training epochs')
-    discovery_group.add_argument('--g2v_min_count', type=int, default=5,
+    discovery_group.add_argument('--g2v_min_count', type=int, default=10,
                                   help='Minimum subgraph frequency to be included in Graph2Vec vocabulary')
     discovery_group.add_argument('--min_states', type=int, default=2,
                                   help='Minimum number of states to test in clustering')
-    discovery_group.add_argument('--max_states', type=int, default=15,
+    discovery_group.add_argument('--max_states', type=int, default=10,
                                   help='Maximum number of states to test in clustering')
-    discovery_group.add_argument('--g2v_umap_dim', type=int, nargs='+', default=[2],
+    discovery_group.add_argument('--g2v_umap_dim', type=int, nargs='+', default=[2, 3, 5, 6, 7, 10],
                                   help='UMAP dimensionalities to sweep for clustering '
                                        '(e.g., --g2v_umap_dim 2 5 10)')
 
